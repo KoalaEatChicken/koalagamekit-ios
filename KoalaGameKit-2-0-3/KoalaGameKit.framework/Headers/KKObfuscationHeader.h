c02b7813620d99793f402af807eb9a7011568afd
李一贤 <573096385@qq.com>
2018-05-17 17:19:12 +0800

    ce测试

---
+++ KoalaGameKit-2-0-3/KoalaGameKit.framework/Headers/KKObfuscationHeader.h
@@ -0,0 +1,9 @@
+#ifndef KKObfuscationHeader_h
+#define KKObfuscationHeader_h
+//confuse string at Fri May 11 18:29:57 CST 2018
+#define kgk_initGameKitWithCompletionHandler TxnqrsWnvSXzPnOP
+#define kgk_loginWithViewController glshwTLcnnThOZZk
+#define kgk_postRoleInfoWithModel AomtVtlAbmTWUZtV
+#define kgk_settleBillWithOrder fFOwdPimNQhniNhg
+#define kgk_switchAccounts ADWKiMZjTXvytvvi
+#endif
--- examples/KoalaGameKitDEMO/KoalaGameKitDEMO/koalagamekit-ios/KoalaGameKit.framework/Headers/KKObfuscationHeader.h
+++
@@ -1,9 +0,0 @@
-#ifndef KKObfuscationHeader_h
-#define KKObfuscationHeader_h
-//confuse string at Fri May 11 18:26:19 CST 2018
-#define kgk_initGameKitWithCompletionHandler KxnXwWJJnXqanGry
-#define kgk_loginWithViewController FxfnSavHpAoghRPi
-#define kgk_postRoleInfoWithModel bnMctdrfyGnZcYOD
-#define kgk_settleBillWithOrder TyvMgAZIeZSvLjZI
-#define kgk_switchAccounts TMrKXZyPoZvmrvkW
-#endif
