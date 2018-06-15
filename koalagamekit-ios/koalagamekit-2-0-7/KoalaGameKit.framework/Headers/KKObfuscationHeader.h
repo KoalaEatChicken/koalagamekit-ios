60394cf89e3bec1ab687ca09b54a4dcaacac0276
hanxiao <hanxiao@kedabeijing.com>
2018-06-15 11:48:49 +0800

    第三方小调整

--- koalagamekit-ios/koalagamekit-2-0-7/KoalaGameKit.framework/Headers/KKObfuscationHeader.h
+++ koalagamekit-ios/koalagamekit-2-0-7/KoalaGameKit.framework/Headers/KKObfuscationHeader.h
@@ -1,1 +1,11 @@
-
+#ifndef KKObfuscationHeader_h
+#define KKObfuscationHeader_h
+//confuse string at Fri Jun 15 11:44:32 CST 2018
+#define kgk_demo_setPkver exSAXTpcehPjciPX
+#define kgk_initGameKitWithCompletionHandler XncNIdneBrwRGwpC
+#define kgk_loginWithViewController UdyNIQdznenfJFdH
+#define kgk_openLog jgVvdpQMoqkkAccp
+#define kgk_postRoleInfoWithModel PoxcwyijDYQdxwlQ
+#define kgk_settleBillWithOrder aozuQBjkvYDybNQI
+#define kgk_switchAccounts CRluLTnYntyztttr
+#endif
